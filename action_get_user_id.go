0feeba07355c81b79c24d0a937e7328d38c6b0f4
Hoang <lequanghoang99@outlook.com>
2021-11-26 17:24:14 +0700

    add GetUserIDByDeviceId

--- action_get_user_id.go
+++ action_get_user_id.go
@@ -18,11 +18,43 @@ func (s Service) GetUserIDByAuthToken(authToken string) (userID string) {
 		}
 	)
 
+	if authToken == "" {
+		return
+	}
+
 	// Find
 	if err := col.FindOne(ctx, cond).Decode(&device); err != nil {
 		logger.Error("devicemngmt - getUserIDByAuthToken", logger.LogData{
 			"authToken": authToken,
 			"err":       err.Error(),
+		})
+		return
+	}
+
+	userID = device.UserID.Hex()
+	return
+}
+
+// GetUserIDByDeviceId ...
+func (s Service) GetUserIDByDeviceId(deviceId string) (userID string) {
+	var (
+		ctx    = context.Background()
+		col    = s.getDeviceCollection()
+		device = Device{}
+		cond   = bson.M{
+			"deviceId": deviceId,
+		}
+	)
+
+	if deviceId == "" {
+		return
+	}
+
+	// Find
+	if err := col.FindOne(ctx, cond).Decode(&device); err != nil {
+		logger.Error("devicemngmt - getUserIDByDeviceId", logger.LogData{
+			"deviceId": deviceId,
+			"err":      err.Error(),
 		})
 		return
 	}
